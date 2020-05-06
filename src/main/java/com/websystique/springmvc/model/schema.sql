50e541e49464818ff719ceccc4a62069f4e03c9f
David Oster <paspa@hotmail.com>
2020-05-06 17:20:33 +0300

    Initial

---
+++ src/main/java/com/websystique/springmvc/model/schema.sql
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  mac
+ * Created: 06-May-2020
+ */
+
+CREATE SCHEMA websystique;
+USE websystique;
+CREATE TABLE EMPLOYEE(
+    id INT NOT NULL auto_increment, 
+    name VARCHAR(50) NOT NULL,
+    joining_date DATE NOT NULL,
+    salary DOUBLE NOT NULL,
+    ssn VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
